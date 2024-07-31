2f53e2a21fcf19cf7e59e32726901e85b3f417a2
Lv-Mveexus <vuyisa66@gmail.com>
2024-07-31 02:10:29 +0200

    Added new code in the api server file to add new expenses

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const bcrypt = require('bcrypt');
 const dotenv = require('dotenv');
 
+// Setting up middleware
 app.use(express.json());
 app.use(cors());
 dotenv.config();
@@ -49,6 +50,25 @@ db.connect((err) => {
 
 				console.log('Users table created/checked');
 			});
+
+			// Create expenses table
+			const expensesTable = `
+            CREATE TABLE IF NOT EXISTS expenses (
+               id INT AUTO_INCREMENT PRIMARY KEY,
+               user_id INT NOT NULL,
+               name VARCHAR(100) NOT NULL,
+               amount DECIMAL(10, 2) NOT NULL,
+               date DATE NOT NULL,
+					type VARCHAR(10) NOT NULL,
+               FOREIGN KEY (user_id) REFERENCES users(id)
+            )
+         `;
+
+			db.query(expensesTable, (err, result) => {
+				if (err) return console.log(err);
+
+				console.log('Expense table created/checked');
+			});
 		});
 	});
 });
@@ -62,12 +82,12 @@ app.post('/api/users/register', async (req, res) => {
 			if (data.length > 0)
 				return res.status(409).json('User already registered');
 
-			// Hashing of the password
+			// Hashing of the users password
 			const salt = bcrypt.genSaltSync(10);
 			const hashedPassword = bcrypt.hashSync(req.body.password, salt);
 
 			const newUser = `INSERT INTO users (email, username, password) VALUES (?)`;
-			value = [req.body.email, re.body.username, hashedPassword];
+			value = [req.body.email, req.body.username, hashedPassword];
 
 			db.query(newUser, [value], (err, data) => {
 				if (err) res.status(400).json('Something went wrong');
@@ -91,11 +111,44 @@ app.post('/api/users/login', async (req, res) => {
 				req.body.password,
 				data[0].password
 			);
-
+			// Check if password is correct
 			if (!passwordVeri)
 				return res.status(400).json('Invalid email or password!');
 
 			return res.status(200).json('login successful');
+		});
+	} catch (err) {
+		res.status(500).json('Internal Server Error');
+	}
+});
+
+//Add new expense route
+app.post('/api/users/expense', async (req, res) => {
+	try {
+		// Check if user is authenthicated
+		const users = `SELECT * FROM USERS WHERE email = ?`;
+		db.query(users, [req.body.email], (err, data) => {
+			if (data.length === 0) return res.status(401).json('Unauthorized');
+
+			// Check all fields are provided
+			if (!req.body.name || !req.body.amount || !req.body.date) {
+				return res.status(400).json('Missing inputs required');
+			}
+
+			// Insert new expense/income into the expenses table
+			const newExpense = `INSERT INTO expense (user_id, name, amount, type, date) VALUES (?,?,?,?,?)`;
+			const values = [
+				data[0].id,
+				req.body.name,
+				req.body.amount,
+				req.body.type,
+			];
+
+			db.query(newExpense, values, (err, result) => {
+				if (err) return res.status(400).json('Something went wrong');
+
+				return res.status(201).json('Expense added successfully');
+			});
 		});
 	} catch (err) {
 		res.status(500).json('Internal Server Error');
